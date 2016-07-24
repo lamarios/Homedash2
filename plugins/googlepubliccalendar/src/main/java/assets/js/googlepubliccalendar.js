f1eabe9c339e18769572e84d823575097710a4f1
Paul Fauchon <lamarios@gmail.com>
2016-07-24 17:39:15 +0100

    add google public calendar module

---
+++ plugins/googlepubliccalendar/src/main/java/assets/js/googlepubliccalendar.js
@@ -0,0 +1,119 @@
+function googlepubliccalendar(moduleId) {
+    this.moduleId = moduleId;
+
+    this.onConnect = function () {
+    };
+
+    this.documentReady = function (size) {
+
+        var parent = this;
+        var root = this.root();
+
+        var modal = this.modal();
+
+        root.on('click', '.gcal-event', function (event) {
+            var tr = $(this);
+            var link = '<a href="' + tr.attr('data-href')
+                       + '" target="_blank">Link to the event</a>';
+
+            modal.find('.event-title').html(tr.attr('data-title'));
+            modal.find('.event-date').html(tr.attr('data-date'));
+            modal.find('.description')
+                .html(parent.url2link(parent.nl2br(tr.attr('data-description')))
+                      + '<br /><br />' + link);
+
+            modal.modal('show');
+        });
+    }
+
+    this.root = function () {
+        return rootElement(this.moduleId);
+    }
+
+    /**
+     * Help function to get the modal of the html
+     * @returns {*|jQuery}
+     */
+    this.modal = function () {
+        var root = this.root();
+        if (root.find('.modal') != undefined) {
+            root.find('.modal').attr('data-module', this.moduleId);
+            root.find('.modal').appendTo("body");
+        }
+
+        return $(document).find('.modal[data-module="' + this.moduleId + '"]');
+    }
+
+    this.onMessage_3x1 = function (command, message, extra) {
+        var root = this.root();
+
+        root.find('.title').html('Next on '+message.title);
+
+        if (message.events.length > 0) {
+            var event = message.events[0];
+            var eventElem = root.find('.gcal-event');
+
+            eventElem.attr('data-title', event.summary);
+            eventElem.attr('data-description', event.description);
+            eventElem.attr('data-href', event.link);
+            eventElem.attr('data-date', event.startTime);
+
+            root.find('.event-title').html(event.summary);
+            root.find('.date').html(event.startTime);
+        }
+    }
+
+    this.onMessage_fullScreen = function (command, message, extra) {
+        this.processMessage(command, message, extra);
+    }
+
+    this.onMessage_3x4 = function (command, message, extra) {
+        this.processMessage(command, message, extra);
+    }
+
+    this.onMessage_4x4 = function (command, message, extra) {
+        this.processMessage(command, message, extra);
+    }
+
+    this.processMessage = function (command, message, extra) {
+        switch (command) {
+            case 'refresh':
+            default:
+                this.processData(message);
+                break;
+        }
+    };
+
+    this.processData = function (message) {
+        var parent = this;
+        var root = this.root();
+        var table = root.find('.events tbody');
+
+        root.find('.title').html(message.title);
+
+        table.html('');
+
+        $.each(message.events, function (index, event) {
+            var html = [];
+
+            html.push('<tr data-date="', event.startTime, '" data-title="', event.summary,
+                      '" data-description="', event.description, '" data-href="', event.link,
+                      '" class="gcal-event">');
+            html.push('<td>', event.startTime, '</td><td>', event.summary, '</td>');
+            html.push('</tr>');
+
+            table.append(html.join(''));
+        });
+    };
+
+    this.nl2br = function (str, is_xhtml) {
+        var breakTag = (is_xhtml || typeof is_xhtml === 'undefined') ? '<br />' : '<br>';
+        return (str + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+    };
+
+    this.url2link = function (text) {
+        var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+        return text.replace(exp, '<a href="$1" target="_blank">$1</a>');
+    };
+
+}
---
+++ plugins/googlepubliccalendar/src/main/java/com/ftpix/homedash/plugins/googlepubliccalendar/GooglePublicCalendarPlugin.java
@@ -0,0 +1,245 @@
+package com.ftpix.homedash.plugins.googlepubliccalendar;
+
+import com.ftpix.homedash.models.ModuleExposedData;
+import com.ftpix.homedash.models.ModuleLayout;
+import com.ftpix.homedash.models.WebSocketMessage;
+import com.ftpix.homedash.plugins.Plugin;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TimeZone;
+
+/**
+ * Created by gz on 24-Jul-16.
+ */
+public class GooglePublicCalendarPlugin extends Plugin {
+
+    private final String CALENDAR_ID = "calendarId", API_KEY = "apiKey";
+    private String calendarId, apiKey, timeZone = TimeZone.getDefault().getID();
+    private SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+    private SimpleDateFormat df2 = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    // private SimpleDateFormat df3 = new SimpleDateFormat("yyyy-MM-dd");
+    private final String URL = "https://www.googleapis.com/calendar/v3/calendars/[ID]/events?orderBy=startTime&singleEvents=true&timeMin=[STARTTIME]&timeZone=[TIMEZONE]&key=[APIKEY]";
+    private String url = "";
+
+
+    @Override
+    public String getId() {
+        return "googlepubliccalendar";
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Google Public Calendar";
+    }
+
+    @Override
+    public String getDescription() {
+        return "View upcoming events from any Google public calendar.";
+    }
+
+    @Override
+    public String getExternalLink() {
+        return null;
+    }
+
+    @Override
+    protected void init() {
+        try {
+            apiKey = settings.get(API_KEY);
+            calendarId = settings.get(CALENDAR_ID);
+            url = URL.replace("[ID]", URLEncoder.encode(calendarId, "UTF-8")).replace("[APIKEY]", apiKey).replace("[TIMEZONE]", URLEncoder.encode(timeZone, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            logger.error("Error while encoding calendar id", e);
+        }
+    }
+
+    @Override
+    public String[] getSizes() {
+        return new String[]{"3x1", "3x4", "4x4", ModuleLayout.FULL_SCREEN};
+    }
+
+    @Override
+    public int getBackgroundRefreshRate() {
+        return 0;
+    }
+
+    @Override
+    protected WebSocketMessage processCommand(String command, String message, Object extra) {
+        return null;
+    }
+
+    @Override
+    public void doInBackground() {
+
+    }
+
+    @Override
+    protected Object refresh(String size) throws Exception {
+
+        try {
+
+            return getCalendar();
+
+        } catch (Exception e) {
+            logger.info("Error while parsing calendar");
+        }
+
+        return null;
+    }
+
+    @Override
+    public int getRefreshRate() {
+        return ONE_HOUR;
+    }
+
+    @Override
+    public Map<String, String> validateSettings(Map<String, String> settings) {
+        try {
+            String apiKey = settings.get(API_KEY);
+            String calendarId = settings.get(CALENDAR_ID);
+            String url = URL.replace("[ID]", URLEncoder.encode(calendarId, "UTF-8")).replace("[APIKEY]", apiKey).replace("[TIMEZONE]", URLEncoder.encode(timeZone, "UTF-8"));
+
+            Calendar today = Calendar.getInstance();
+            today.set(Calendar.HOUR, 0);
+            today.set(Calendar.MINUTE, 0);
+            today.set(Calendar.SECOND, 0);
+            url = url.replace("[STARTTIME]", URLEncoder.encode(df.format(today.getTime()), "UTF-8"));
+
+            Unirest.get(url).asString().getBody().toString();
+        } catch (Exception e) {
+            Map<String, String> result = new HashMap<>();
+            result.put("Calendar not available", "The calandar you set is not reachable for the following reason '" + e.getMessage() + "'");
+            return result;
+        }
+        return null;
+    }
+
+    @Override
+    public ModuleExposedData exposeData() {
+        try {
+            ModuleExposedData data = new ModuleExposedData();
+            GoogleCalendar calendar;
+            calendar = getCalendar();
+            if (calendar.events.size() > 0) {
+                GoogleCalendarEvent event = calendar.events.get(0);
+                data.addText(calendar.title);
+                data.addText(event.startTime);
+                data.addText(event.summary);
+
+                return data;
+            }
+
+            return null;
+        } catch (Exception e) {
+            logger.error("Couldn't expose google calendar data", e);
+            return null;
+        }
+    }
+
+    @Override
+    public Map<String, String> exposeSettings() {
+        Map<String, String> result = new HashMap<>();
+
+        try {
+            String apiKey = settings.get(API_KEY);
+            String calendarId = settings.get(CALENDAR_ID);
+            String url = URL.replace("[ID]", URLEncoder.encode(calendarId, "UTF-8")).replace("[APIKEY]", apiKey).replace("[TIMEZONE]", URLEncoder.encode(timeZone, "UTF-8"));
+
+            Calendar today = Calendar.getInstance();
+            today.set(Calendar.HOUR, 0);
+            today.set(Calendar.MINUTE, 0);
+            today.set(Calendar.SECOND, 0);
+            url = url.replace("[STARTTIME]", URLEncoder.encode(df.format(today.getTime()), "UTF-8"));
+
+            try {
+                JSONObject json = new JSONObject(Unirest.get(url).asString().getBody().toString());
+
+                result.put("Calendar", StringEscapeUtils.escapeHtml4(json.getString("summary")));
+
+            } catch (Exception e) {
+                logger.error("Can't expose settings for goolge calendar", e);
+            }
+
+            return result;
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
+    /// plugin methods
+    private GoogleCalendar getCalendar() throws JSONException, IOException, ParseException, UnirestException {
+        Calendar today = Calendar.getInstance();
+        today.set(Calendar.HOUR, 0);
+        today.set(Calendar.MINUTE, 0);
+        today.set(Calendar.SECOND, 0);
+
+        String url = this.url.replace("[STARTTIME]", URLEncoder.encode(df.format(today.getTime()), "UTF-8"));
+
+        JSONObject json = new JSONObject(Unirest.get(url).asString().getBody().toString());
+        GoogleCalendar calendar = new GoogleCalendar();
+        calendar.title = StringEscapeUtils.escapeHtml4(json.getString("summary"));
+        calendar.description = StringEscapeUtils.escapeHtml4(json.getString("description"));
+
+        JSONArray jsonarray = json.getJSONArray("items");
+
+        for (int i = 0; i < jsonarray.length(); i++) {
+
+            JSONObject jsonEvent = jsonarray.getJSONObject(i);
+
+            GoogleCalendarEvent event = new GoogleCalendarEvent();
+            try {
+                event.summary = StringEscapeUtils.escapeHtml4(jsonEvent.getString("summary"));
+            } catch (Exception e) {
+            }
+
+            try {
+                event.description = StringEscapeUtils.escapeHtml4(jsonEvent.getString("description"));
+            } catch (Exception e) {
+            }
+
+            try {
+                event.link = jsonEvent.getString("htmlLink");
+            } catch (Exception e) {
+            }
+            try {
+                Date date = df.parse(jsonEvent.getJSONObject("start").getString("dateTime"));
+                event.startTime = df2.format(date);
+            } catch (JSONException e) {
+                event.startTime = jsonEvent.getJSONObject("start").getString("date");
+            }
+
+            calendar.events.add(event);
+
+        }
+
+        return calendar;
+    }
+
+    //////inner classes
+    private class GoogleCalendarEvent {
+        public String summary, description, startTime, link;
+    }
+
+    private class GoogleCalendar {
+        public String title, description;
+        public List<GoogleCalendarEvent> events = new ArrayList<GoogleCalendarEvent>();
+    }
+}
